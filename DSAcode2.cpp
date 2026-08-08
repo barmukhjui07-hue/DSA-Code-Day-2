5e1dc3a8d2c6f49b6ebc5d09685509acadc34278
Jui Barmukh <barmukh.jui07@gmail.com>
2026-08-08 18:24:17 +0530

    Add files via upload

---
+++ DSAcode2.cpp
@@ -0,0 +1,32 @@
+// Opearators and variables:
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a = 10, b = 5;
+    cout<<"sum= " << (a+b) <<endl;
+    cout<<"difference= " << (a-b) <<endl;
+    cout<<"product= " << (a*b) <<endl;
+    cout<<"division= " << (a/b) <<endl;
+    cout<<"modulo= " << (a%b) <<endl;
+    return 0;
+}*/
+// Increment:
+/*#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = ++a;
+    cout << "b = " << b << endl;
+    cout << "a = " << a << endl;
+    return 0;
+}*/
+// Decrement:
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = a--;
+    cout << "b = " << b << endl;
+    cout << "a = " << a << endl;
+    return 0;
+}
